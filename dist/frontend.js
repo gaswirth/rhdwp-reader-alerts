2ac0c25157d5f4b4f031d44261662d2750b328af
Nick Gaswirth <nick@roundhouse-designs.com>
2019-09-05 02:07:42 +0000

    set user cookie on link follow

--- dist/frontend.js
+++ dist/frontend.js
@@ -6,44 +6,59 @@
  * @uses js-cookie
  */
 
-;(function ($) {
-  const cookie_prefix = 'rhdwp_reader_alert__'
+(function($) {
+	const cookie_prefix = 'rhdwp_reader_alert__';
 
-  function removeDismissedAlerts () {
-    $('.reader-alert-item').each(function () {
-      const id = $(this).attr('data-post-id')
+	function removeDismissedAlerts() {
+		$('.reader-alert-item').each(function() {
+			const id = $(this).attr('data-post-id');
 
-      if (Cookies.get(`${cookie_prefix}${id}`)) {
-        $(this).remove()
-      }
-    })
-  }
+			if (Cookies.get(`${cookie_prefix}${id}`)) {
+				$(this).remove();
+			}
+		});
+	}
 
-  function clickActions () {
-    $('.close-reader-alert').click(function (e) {
-      e.preventDefault()
-      const item = $(this).parents('.reader-alert-item')
-      const id = item.attr('data-post-id')
+	/**
+	 * Sets a cookie so the user won't see a repeat alert after closing/following the link.
+	 *
+	 * @param {string} id
+	 */
+	function setUserCookie(id) {
+		Cookies.set(`${cookie_prefix}${id}`, Date.now(), { expires: 7 });
+	}
 
-      item.animate(
-        {
-          height: 0
-        },
-        'fast',
-        function () {
-          $(this).remove()
-        }
-      )
+	function clickActions() {
+		$('.close-reader-alert').click(function(e) {
+			e.preventDefault();
 
-      Cookies.set(`${cookie_prefix}${id}`, Date.now(), { expires: 7 })
-    })
-  }
+			const item = $(this).parents('.reader-alert-item');
+			const id = item.attr('data-post-id');
 
-  $(document).ready(function () {
-    removeDismissedAlerts()
+			item.animate(
+				{
+					height: 0
+				},
+				'fast',
+				function() {
+					$(this).remove();
+					setUserCookie(id);
+				}
+			);
+		});
 
-    $('.reader-alerts').animate({ opacity: 1 })
+		$('.reader-alert-link').click(function(e) {
+			setUserCookie(id);
 
-    clickActions()
-  })
-})(jQuery)
+			location.href = $(this).attr('href');
+		});
+	}
+
+	$(document).ready(function() {
+		removeDismissedAlerts();
+
+		$('.reader-alerts').animate({ opacity: 1 });
+
+		clickActions();
+	});
+})(jQuery);
